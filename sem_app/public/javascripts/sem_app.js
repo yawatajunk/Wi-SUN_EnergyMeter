f8c6d61991939e00f4661f8d2e6615081b261235
Pi <pi@blue-black.ink>
2016-08-13 13:40:57 +0900

    (node.js + express  + socket.io)によるWebページ追加

---
+++ sem_app/public/javascripts/sem_app.js
@@ -0,0 +1,167 @@
+//
+// sem_app.js
+// 
+// Copyright (C) 2016 pi@blue-black.ink
+//
+
+
+// Websocket address
+const WEBSOCKET_ADDRESS = 'raspi2.local:3610'
+
+
+var pow_int;
+var socketio = io.connect(WEBSOCKET_ADDRESS);
+socketio.on("inst-power", function (data) {
+	var pow_str = data.value;
+	pow_int = Number(pow_str);
+
+	if (pow_int < 2000) {
+		$(function () {
+			$("#progress-bar").toggleClass('progress-bar-success', true);
+			$("#progress-bar").toggleClass('progress-bar-warning', false);
+			$("#progress-bar").toggleClass('progress-bar-danger', false);
+			$("#inst-power").css('color', 'green');
+		});
+	}
+	else if (pow_int < 4000) {
+		$(function () {
+			$("#progress-bar").toggleClass('progress-bar-success', false);
+			$("#progress-bar").toggleClass('progress-bar-warning', true);
+			$("#progress-bar").toggleClass('progress-bar-danger', false);
+			$("#inst-power").css('color', 'orange');
+		});
+	}
+	else {
+		$(function () {
+			$("#progress-bar").toggleClass('progress-bar-success', false);
+			$("#progress-bar").toggleClass('progress-bar-warning', false);
+			$("#progress-bar").toggleClass('progress-bar-danger', true);
+			$("#inst-power").css('color', 'red');
+		});
+	}
+
+	$(function () {
+		$("#inst-power").text(pow_str + ' W')
+	});
+	
+	var gain_int = Math.round(pow_int / 6000.0 * 100.0);	// 6000 W -> 100 %
+	if (gain_int > 100.0) {
+		gina_int = 100.0;
+	} 
+	var gain_str = String(gain_int) + '%';
+	$(function () {
+		$("#progress-bar").css('width', gain_str);
+	});
+});	// end of socket.on()
+
+
+// highcharts
+$(function () {
+
+	$('#chart-container').highcharts({
+
+		chart: {
+			type: 'gauge',
+			plotBackgroundColor: null,
+			plotBackgroundImage: null,
+			plotBorderWidth: 0,
+			plotShadow: false,
+
+			events : {
+				load: function () {
+					var point = this.series[0].points[0]
+					setInterval(function () {
+						point.update(pow_int);
+					}, 500);
+				}
+			}
+		},
+		
+		credits: {
+			enabled: false
+		},
+			
+		title: {
+			text: 'Instant Power'
+		},
+
+		pane: {
+			startAngle: -150,
+			endAngle: 150,
+			background: [{
+				backgroundColor: {
+					linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+					stops: [
+						[0, '#FFF'],
+						[1, '#333']
+					]
+				},
+				borderWidth: 0,
+				outerRadius: '100%'
+			}, {
+				backgroundColor: {
+					linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+					stops: [
+						[0, '#333'],
+						[1, '#FFF']
+					]
+				},
+				borderWidth: 0,
+				outerRadius: '100%'
+			}, {
+				// default background
+			}, {
+				backgroundColor: '#DDD',
+				borderWidth: 0,
+				outerRadius: '100%',
+				innerRadius: '100%'
+			}]
+		},
+
+		// the value axis
+		yAxis: {
+			min: 0,
+			max: 6000,
+
+			minorTickInterval: 'auto',
+			minorTickWidth: 1,
+			minorTickLength: 10,
+			minorTickPosition: 'inside',
+			minorTickColor: '#666',
+
+			tickPixelInterval: 30,
+			tickWidth: 2,
+			tickPosition: 'inside',
+			tickLength: 10,
+			tickColor: '#666',
+			labels: {
+				step: 2,
+				rotation: 'auto'
+			},
+			title: {
+				text: 'Watt'
+			},
+			plotBands: [{
+				from: 0,
+				to: 2000,
+				color: '#55BF3B' // green
+			}, {
+				from: 2000,
+				to: 4000,
+				color: '#DDDF0D' // yellow
+			}, {
+				from: 4000,
+				to: 6000,
+				color: '#DF5353' // red
+			}]
+		},
+
+		series: [{
+			name: 'Power',
+			data: [0],
+			tooltip: {
+				valueSuffix: ' [W]'
+			}
+		}]				
+	});
+});
