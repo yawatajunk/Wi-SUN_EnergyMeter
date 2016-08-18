9fba9fed329d728a150a83a46e87bca26c11b909
pi <pi@blue-black.ink>
2016-08-18 15:48:54 +0900

    some changes

---
+++ user_conf.py
@@ -0,0 +1,13 @@
+# coding: UTF-8
+#
+# user_conf.py
+#
+# スマート電力量メーター　ユーザ設定
+#
+# Copyright(C) 2016 pi@blue-black.ink
+#
+
+SEM_ROUTEB_ID = '00000000000000000000000000000000'
+SEM_PASSWORD = 'XXXXXXXXXXXX'
+SEM_INTERVAL = 3
+SEM_DURATION = 6
